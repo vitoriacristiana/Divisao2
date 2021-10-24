15e055a987f7d69d68e30ca6d469eb57c7dc2915
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-10-24 19:56:49 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Divisao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerador, denominador;
+            double resultado;
+
+            Console.WriteLine("Bem vindo ao calculo de Divisao.\n");
+
+            Console.Write("Digite o numerador..: ");
+            numerador = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o denominador: ");
+            denominador = Convert.ToInt32(Console.ReadLine());
+
+            if (denominador == 0)
+            {
+            Console.WriteLine("\n Não é possível dividir por 0.");
+            }
+            else
+            {
+            resultado = numerador / denominador;
+
+             Console.WriteLine($"\n{numerador} dividido {denominador} é {resultado}");
+             Console.ForegroundColor = ConsoleColor.DarkMagenta;
+             
+            }
+        }
+    }
+}
